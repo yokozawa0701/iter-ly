a11aee005d18782e845574295c8857b8346dd462
yokozawa0701 <d.yoko.0701@gmail.com>
2026-01-31 16:54:55 +0900

    parts コンポーネントとプレビューページの作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yokozawa0701.github.io',
+  site: 'https://maaji-co-jp.github.io',
   base: '/iter-ly',
   vite: {
     plugins: [tailwindcss()],
