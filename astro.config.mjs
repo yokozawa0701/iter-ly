59ec19f9f80266fe95e141352b6f855fa9b0c623
yokozawa0701 <d.yoko.0701@gmail.com>
2026-02-03 01:58:26 +0900

    astro-icon と lucide アイコンライブラリを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maaji-co-jp.github.io',
   base: '/iter-ly',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [icon()],
 });
